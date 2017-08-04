36adddb02719c4a95e23842d352862c74b94d6a3
Shaun <shaun.hall2012@gmail.com>
2017-08-04 14:01:16 -0400

    bug in my code

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,11 +4,11 @@ module.exports = (app) => {
 
     app.get(
         '/auth/google',
-        passport.authenticate('google',  { 
+        passport.authenticate('google', { 
         scope: ['profile', 'email']
     })
     );
 
-    app.get('/auth/google/callback', passport.authenticate('google'));
+    app.get('/auth/google/callback', passport.authenticate('google'))
 
-}
+};

641ad08ebc5c88f2f315235c711a67422630bf1f
Shaun <shaun.hall2012@gmail.com>
2017-08-04 01:31:06 -0400

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require ('express');
+require ('./services/passport');
+
+
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'Kai' });
-});
+require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
